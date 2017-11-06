d37810703e0ae139329d237240a14b1b773c0935
Abraão Santana <deploys.app@gmail.com>
2017-11-06 19:24:25 -0200

    Commit 5

--- ExpressoTavares/settings.py
+++ ExpressoTavares/settings.py
@@ -77,9 +77,10 @@ USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 
-STATIC_URL = '/static/'
-
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
